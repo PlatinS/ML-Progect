087e4830095d58747062ebcf442b4c55ef30bc54
PlatinS <antonych@yandex.ru>
2020-08-02 13:30:50 +0300

    Рабочий вариант 1

--- run_server.py
+++ run_server.py
@@ -1,15 +1,15 @@
+from flask import Flask, render_template, jsonify, request
+import logging
+import traceback
+from logging.handlers import RotatingFileHandler
+from time import time, strftime
 import os
-import pandas as pd
 import dill
+
 dill._dill._reverse_typemap['ClassType'] = type
 
-import flask
-import logging
-from logging.handlers import RotatingFileHandler
-from time import strftime
+app = Flask(__name__)
 
-# initialize our Flask application and the model
-app = flask.Flask(__name__)
 model = None
 
 handler = RotatingFileHandler(filename='app.log', maxBytes=100000, backupCount=10)
@@ -23,54 +23,37 @@ def load_model(model_path):
 	with open(model_path, 'rb') as f:
 		model = dill.load(f)
 
-modelpath = "./models/pipeline-nlp.dill"
+modelpath = "./models/model_nlp.dill"
 load_model(modelpath)
 
-@app.route("/", methods=["GET"])
-def general():
-	return """Welcome to fraudelent prediction process. Please use 'http://<address>/predict' to POST"""
+@app.route("/")
+def index():
+	return "API for predict service"
 
-@app.route("/predict", methods=["POST"])
+@app.route("/predict", methods=['POST'])
 def predict():
-	# initialize the data dictionary that will be returned from the
-	# view
-	data = {"success": False}
-	dt = strftime("[%Y-%b-%d %H:%M:%S]")
-	# ensure an image was properly uploaded to our endpoint
-	if flask.request.method == "POST":
+	json_input = request.json
 
-		description, company_profile, benefits = "", "", ""
-		request_json = flask.request.get_json()
-		if request_json["description"]:
-			description = request_json['description']
+	current_datatime = strftime('[%Y-%b-%d %H:%M:%S]')
+	ip_address = request.headers.get("X-Forwarded-For", request.remote_addr)
+	logger.info(f'{current_datatime} request from {ip_address}: {request.json}')
+	start_prediction = time()
 
-		if request_json["company_profile"]:
-			company_profile = request_json['company_profile']
+	tweet = json_input['tweet']
 
-		if request_json["benefits"]:
-			benefits = request_json['benefits']
-		logger.info(f'{dt} Data: description={description}, company_profile={company_profile}, benefits={benefits}')
-		try:
-			preds = model.predict_proba(pd.DataFrame({"description": [description],
-												  "company_profile": [company_profile],
-												  "benefits": [benefits]}))
-		except AttributeError as e:
-			logger.warning(f'{dt} Exception: {str(e)}')
-			data['predictions'] = str(e)
-			data['success'] = False
-			return flask.jsonify(data)
+	result = {
+        'ID': tweet,
+        'value_Poisson': 1,
+        'value_Gamma': 2,
+        'value_BurningCost': 'Yesss!'
+    }
 
-		data["predictions"] = preds[:, 1][0]
-		# indicate that the request was a success
-		data["success"] = True
+	end_prediction = time()
+	duration = round(end_prediction - start_prediction, 6)
+	current_datatime = strftime('[%Y-%b-%d %H:%M:%S]')
+	logger.info(f'{current_datatime} predicted for {duration} msec: {result}\n')
 
-	# return the data dictionary as a JSON response
-	return flask.jsonify(data)
+	return jsonify(result)
 
-# if this is the main thread of execution first load the model and
-# then start the server
-if __name__ == "__main__":
-	print(("* Loading the model and Flask starting server..."
-		"please wait until server has fully started"))
-	port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', debug=True, port=port)
+if __name__ == '__main__':
+	app.run(debug = True)
