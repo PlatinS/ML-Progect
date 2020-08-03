1311f4ff651c160418e24b6d6ecba4437b40b53d
PlatinS <antonych@yandex.ru>
2020-08-03 12:49:05 +0300

    Проверка работы сервиса. Отправка POST и получение результата.

--- postman.py
+++ postman.py
@@ -4,13 +4,11 @@ data = {
     "tweet": "Coronavirus cases around the globe have surpassed 17.8 million. More than 4.6 million infections have been recorded in the United States."
     }
 
-
 def send_json(data):
     url = 'http://127.0.0.1:5000/predict'
     headers = {'content-type': 'application/json'}
     response = requests.post(url, json=data, headers=headers)
     return response
-
 
 if __name__ == '__main__':
     response = send_json(data)
